827411fd933693c123cc3381c851fd91f70982a5
tetroborat <muchomedyanov@ya.ru>
2021-06-14 16:11:02 +0300

    Один и надеюсь единственный комит: -главная страница и вся фигня на ней

--- graduation/settings.py
+++ graduation/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
